aff1b3de17a9f5918f076f160a7f0e110a9c23b5
Nnisik <nisik0120@gmail.com>
2024-01-21 22:04:41 +0300

    removed html-folder

--- auth-user.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (isset($_GET["id"])) {
-        require_once "./html/auth-user-elements.html";
-    }
-    else {
-        require_once "./html/non-auth-user-elements.html";
-    }
-?>
